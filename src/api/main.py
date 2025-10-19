6bec0966a584cee18044097f0e7a8e262a4a1100
Keigo Healy <afkhealy@gmail.com>
2025-10-19 16:44:42 -0500

    template fin

--- src/api/main.py
+++ src/api/main.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+@app.route("/health")
+def health_check():
+    return "OK", 200
+
